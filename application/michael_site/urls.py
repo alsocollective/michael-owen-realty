e3880f2ee7420b75fa639c3945323df3b96fb059
bohdan.anderson@gmail.com <bohdan.anderson@gmail.com>
2014-08-28 14:08:23 -0400

    added other pages

--- application/michael_site/urls.py
+++ application/michael_site/urls.py
@@ -5,6 +5,9 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),	
     url(r'^$', 'michael_site.views.home', name='home'),
+    url(r'^sell/', 'michael_site.views.sell', name='home'),
+	url(r'^buy/', 'michael_site.views.buy', name='home'),
+	url(r'^search/', 'michael_site.views.search', name='home'),
 )
 
 from django.conf import settings
