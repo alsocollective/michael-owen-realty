3be1be6a33272a79cadacb9d6ac68bc7920be611
symonoliver <symonoliver@lisamacdonalds-MacBook-Pro-4.local>
2014-12-17 16:02:40 -0500

    making page title dynamic

--- application/michael_site/views.py
+++ application/michael_site/views.py
@@ -51,7 +51,8 @@ def getFeatured():
 
 def home(request):
 	out = AboutPage.objects.all().order_by("created")[0]
-	return render_to_response('about.html',{"pagecontent":out,"MEDIA_URL":MEDIA_URL,'basetemplate':templateType(request),"featured":getFeatured(),'pageType':getCssClass(request), 'pageTitle':request})
+	page = "Home"
+	return render_to_response('about.html',{"pagecontent":out,"MEDIA_URL":MEDIA_URL,'basetemplate':templateType(request),"featured":getFeatured(),'pageType':getCssClass(request), 'pageTitle':page})
 
 def sell(request):	
 	out = SellPage.objects.all().order_by("created")[0]	
