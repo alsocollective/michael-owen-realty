f217071fd47fa29a4badc50314bf238a16093967
symonoliver <symonoliver@lisamacdonalds-MacBook-Pro-4.local>
2014-12-17 16:42:48 -0500

    dynamic title

--- application/static/js/main.js
+++ application/static/js/main.js
@@ -71,10 +71,6 @@ var app = {
 				app.nav.changeurl(location);
 			}
 
-			console.log("U R L"+url)
-			document.getElementById('main-title').innerHTML = "Michael Owen Real Estate:"+url;
-
-
 			// url = app.url.addajax(url);
 			var newMainContainer = document.createElement("div");
 			newMainContainer.className = "mainwrapper offright";
@@ -107,6 +103,8 @@ var app = {
 		changeurl: function(link) {
 			$(".currentpage").removeClass("currentpage");
 			$($('a[href="' + link + '"]')[0].parentNode).addClass("currentpage")
+			console.log("U R L"+link);
+			//document.getElementById('main-title').innerHTML = "Michael Owen Real Estate:"+url;
 		},
 		correctSize: function() {
 			$("#nav").width($($(".mainwrapper div")[0]).width());
